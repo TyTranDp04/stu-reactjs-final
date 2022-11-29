a418007fa1e289561eaf56ad4557b2e88b99f13c
TyTranDp04 <ty.tran_dp04@devplus.edu.vn>
2022-11-29 23:53:08 +0700

    Implement content/#11 layout

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,10 +1,32 @@
 import React from 'react'
-import { SidebarCol } from './style'
-
+import { SidebarCategory, SidebarCol, SidebarDesc, SidebarInner } from './style'
+import { Link } from 'react-router-dom'
 const Sidebar = () => {
+  const accountRouter = {
+    dashboard: { name: "Dashboard", url: "/dashboard", icon: "" },
+    requests: { name: "Requests", url: "/requests", icon: "" },
+    daysoff: { name: "Days off", url: "/days-off", icon: "" },
+  };
+  const managerRouter = {
+    members: { name: "Members", url: "/members", icon: "" },
+    groups: { name: "Groups", url: "/groups", icon: "" },
+    notifications: { name: "Notifications", url: "/notifications", icon: "" },
+    sync: { name: "Sync", url: "/sync", icon: "" },
+  };
+
   return (
-    <SidebarCol className='col-sm-4'>
-      
+    <SidebarCol className='col-sm-3 col-lg-2'>
+      <SidebarInner>
+        {Object.entries(accountRouter).map(([index, value]) => <SidebarDesc key={index}><Link to={value.url}>{value.name}</Link></SidebarDesc>)}
+      </SidebarInner>
+      <SidebarInner>
+        <SidebarCategory>Manager</SidebarCategory>
+        {Object.entries(managerRouter).map(([index, value]) => <SidebarDesc key={index}><Link to={value.url}>{value.name}</Link></SidebarDesc>)}
+      </SidebarInner>
+      <SidebarInner>
+        <SidebarCategory>Administrator</SidebarCategory>
+        <SidebarDesc><Link to='/workspaces'>Workspaces</Link></SidebarDesc>
+      </SidebarInner>
     </SidebarCol>
   )
 }
